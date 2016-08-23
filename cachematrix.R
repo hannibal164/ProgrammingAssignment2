b1e67e55041065e70d6b43cfeea3235c22e84107
hannibal164 <mmcbride164@hotmail.com>
2016-08-23 18:58:50 +0200

    Solution
    Added in my solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## So what should happen here is makeCacheMatrix creates the object invm, and then cacheSolve checks to see if this exists. If it is, it returns it.
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  invm <- NULL
+  set <- function(y) {
+    x <<- y
+    invm <<- NULL
+  }
+  get <- function() x
+  setting.inv <- function(solve) invm <<- solve
+  get.inv <- function() invm
+  list(set = set, get = get,
+       setting.inv = setting.inv,
+       get.inv = get.inv)
 
 }
 
 
-## Write a short comment describing this function
+## The next function checks to see if the inverse is cached for the object. 
+# If it is, then it returns that inverse, otherwise it calculates the inverser
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cachemean <- function(x, ...) {
+    invm <- x$get.inv()
+    if(!is.null(invm)) {
+      message("getting cached data")
+      return(invm)
+    }
+    data <- x$get()
+    invm <- solve(data, ...)
+    x$setting.inv(invm)
+    invm
+  }
 }
